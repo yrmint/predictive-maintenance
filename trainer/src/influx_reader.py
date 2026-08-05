7033679b07d5538bc740faf098b5bb83c743d33d
yrmint <zhilkinal@yandex.ru>
2026-08-05 13:06:51 +0300

    fix lint errors

--- trainer/src/influx_reader.py
+++ trainer/src/influx_reader.py
@@ -1,10 +1,9 @@
-import os
 
 import pandas as pd
+from config import TrainingConfig
 from influxdb_client import InfluxDBClient
 
 from shared import influxdb_config
-from config import TrainingConfig
 
 
 def load_data() -> pd.DataFrame:
